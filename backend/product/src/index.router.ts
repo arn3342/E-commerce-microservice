13a23a540723252ac73f73f46d48f88cca58fd5b
Aousaf Rashid <nabilrashid44@gmail.com>
2024-05-09 17:10:53 +0600

    Update index.router.ts

--- backend/product/src/index.router.ts
+++ backend/product/src/index.router.ts
@@ -1,1 +1,9 @@
 
+import { Router } from "express";
+import productRouter from "./apis/v1/router/product.router"
+
+const router = Router()
+
+router.use("/v1", productRouter)
+
+export default router
